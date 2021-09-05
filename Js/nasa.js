dbca72710a3e83d29874463570e3b6d5c8f1042c
Hyan Araujo <hyan.araujoalmeida@gmail.com>
2021-09-05 15:55:32 -0300

    agora vai

--- Js/nasa.js
+++ Js/nasa.js
@@ -1,2 +1,16 @@
-const data =
-const button =
+const ola = new XMLHttpRequest()
+ const data = document.getElementById('date'),
+ const button = document.querySelector('btt'),
+ const foto =document.getElementById ('src')
+
+
+$('exp').click(function() {
+    let Api = `https://api.nasa.gov/planetary/apod?tL7ZrourDVQMzSsnwVdqEKTgadpJpHJG5artBOWw=${result}`
+    let result = document.getElementById ("date").value 
+    $.ajax({
+        url: Api,
+        method: 'GET',
+        sucess:
+    })
+
+})
